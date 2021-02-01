42c586176b6dbfdd5aed373f3e69f275307265c2
Eftykhar Rahman <eftykharraufu@gmail.com>
2021-02-01 10:11:09 +0600

    General Function

---
+++ functionGeneral.js
@@ -0,0 +1,14 @@
+// function addTodo(task) {
+//     const newTaskElement = document.getElement('li');
+// }
+
+var name = "Raufu";
+
+function addUser() {
+    console.log(name);
+}
+addUser();
+
+var myTodo = function(task) {
+
+};
